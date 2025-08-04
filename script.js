13af99f52608ba7c4f7bc1d7bfc6f44db00e59f3
Polyana <polyana.moreira.santos@escola.pr.gov.br>
2025-08-04 09:43:03 -0300

    cenario-passo0.png

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const avanca = document.querySelectorAll(‘.btn-proximo’);
+ const avanca = document.querySelectorAll(‘.btn-proximo’);
+
  avanca.forEach(button => {
-    button.addEventListener('click',function(){
+  button.addEventListener('click',function(){
     const atual = document.querySelector(‘.ativo’);
-const proximoPasso = 'passo-' + this.getAttribute ('data-proximo');
-atual.classList.remove('ativo');
-document.getElementByid(proximoPasso).classList.add('ativo');
+    const proximoPasso = 'passo-' + this.getAttribute ('data-proximo');
+
+    atual.classList.remove('ativo');
+    document.getElementByid(proximoPasso).classList.add('ativo');
+
 })
-});
+})

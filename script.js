d3841deeff760899c0b2f2d914dc62c0a53fe0ce
Polyana <polyana.moreira.santos@escola.pr.gov.br>
2025-08-20 08:04:57 -0300

    ultima alteração

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
  avanca.forEach(button => {
-  button.addEventListener('click',function(){
-     const atual = document.querySelector('.ativo');
-    const proximoPasso = 'passo-' + this.getAttribute ('data-proximo');
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-    atual.classList.remove('ativo');
-    document.getElementByid(proximoPasso).classList.add('ativo');
-  })
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
 })
